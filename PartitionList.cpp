38f81c683a404906fbf18be81b1c5be427a76790
siyuqtt <siqiu92@gmail.com>
2014-09-05 09:25:41 -0400

    Update PartitionList.cpp
    point *a;
    point *b;
    a = b; // a and b pointing to the same memory address
    a = *b; // a has the value stored in the memory pointed by b

--- PartitionList.cpp
+++ PartitionList.cpp
@@ -9,37 +9,24 @@
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-        if(head == nullptr) return head;
-        ListNode * less = nullptr,*lesshead = nullptr;
-        ListNode * greater = nullptr,*greaterhead = nullptr;
-        while( head->next != nullptr){
-            if(head->val < x){
-                if(less == nullptr){
-                    less = head;
-                    lesshead = less;
-                }else{
-                    less->next = head;
-                    less = less->next;
-                }
-            }else{
-                if(greater == nullptr){
-                    greater = head;
-                    greaterhead = greater;
-                }else{
-                    greater->next = head;
-                    greater = greater->next;
-                }
+        ListNode dump1(-1), dump2(-1);
+        ListNode * less = &dump1;
+        ListNode * greater = &dump2;
+        while ( head != nullptr ) {
+            if ( head->val < x ) {
+                less->next = head;
+                less = less->next;
+                
+            } else {
+                greater->next = head;
+                greater = greater->next;
             }
             head = head->next;
         }
         
-        greater = nullptr;
-        if(lesshead != nullptr) {
-            less->next = greaterhead;
-            return lesshead;
-        }else{
-            return greaterhead;
-        }
+        greater->next = nullptr;
+        less->next = dump2.next;
+        return dump1.next;
         
     }
 };
