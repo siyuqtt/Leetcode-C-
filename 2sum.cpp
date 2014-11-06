414249e0f104dcc8c13639b2e4e4412ce2d47044
Siyu Qiu <siqiu92@gmail.com>
2014-11-06 11:34:23 -0500

    Update 2sum.cpp
    map.find o(logn)?

--- 2sum.cpp
+++ 2sum.cpp
@@ -3,11 +3,13 @@ public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         vector<int> result;
         if(numbers.size() < 2) return result;
+        unordered_map<int, int> dic;// number idx
+        for(int i = 0; i<numbers.size(); i++) dic[numbers[i]] = i + 1;
         for(int i = 0 ; i < numbers.size(); i++){
             int comple = target - numbers[i];
-            auto vit = find(numbers.begin(), numbers.end(), comple);
-            if(vit != numbers.end()){
-                int idx = distance(numbers.begin(), vit) + 1;
+            auto vit = dic.find(comple);
+            if(vit != dic.end()){
+                int idx = vit->second;
                 if(idx > i+1){
                     result.push_back(i+1);
                     result.push_back(idx);
